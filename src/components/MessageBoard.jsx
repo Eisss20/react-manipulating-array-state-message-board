94fb5f5ed6b6bcdf01e234da3ef573b69e431f82
thanapakorn <thanapakorn001@gmail.com>
2024-10-24 15:28:50 +0700

    feat: add answer

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,23 +1,51 @@
+import { useState } from "react";
+
 function MessageBoard() {
+  const [messages, setMessages] = useState([]);
+  const [inputMessage, setInputMessage] = useState("");
+
+  const handleInputMessage = (e) => {
+    e.preventDefault();
+
+    const newMessage = {
+      id: messages.length + 1,
+    };
+    setMessages([...messages, newMessage]);
+    setInputMessage("");
+  };
+
+  const handleDeleteMessage = (id) => {
+    const updatedMessages = messages.filter((message) => message.id !== id);
+    setMessages(updatedMessages);
+  };
+
   return (
     <div className="app-wrapper">
-      <h1 class="app-title">Message board</h1>
-      <div class="message-input-container">
+      <h1 className="app-title">Message Board</h1>
+      <form onSubmit={handleInputMessage} className="message-input-container">
         <label>
           <input
+            value={inputMessage}
+            onChange={(e) => setInputMessage(e.target.value)}
             id="message-text"
             name="message-text"
             type="text"
             placeholder="Enter message here"
           />
         </label>
-        <button className="submit-message-button">Submit</button>
-      </div>
-      <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+        <button type="submit" className="submit-message-button">
+          Submit
+        </button>
+      </form>
+      <div className="board">
+        {messages.map((message) => (
+          <div key={message.id}>
+            <h1>{message.message}</h1>
+            <button onClick={() => handleDeleteMessage(message.id)}>
+              x
+            </button>{" "}
+          </div>
+        ))}
       </div>
     </div>
   );
