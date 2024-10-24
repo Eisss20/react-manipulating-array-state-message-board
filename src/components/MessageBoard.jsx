6102a4836911576ac3a8cf6835a69198d2b99dcc
Eisss20 <pajenan.j@gmail.com>
2024-10-24 16:25:40 +0700

    Feat:Add answer array state

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,4 +1,26 @@
+
+import { useState } from "react";
+
 function MessageBoard() {
+const [boxArrayUser, setboxArrayUser] = useState([]);
+const [inputUser, setInputUser] = useState(""); 
+
+const handleInputUser = (event) => {
+  setInputUser(event.target.value)
+}
+
+const handleBoxUser = () => {
+  const NewinputUser = [...boxArrayUser]
+  NewinputUser.push(inputUser)
+  setboxArrayUser(NewinputUser) 
+}
+
+const deleteTheBox = (index) => {
+  const NewinputUser = [...boxArrayUser]
+  NewinputUser.splice(index, 1)
+  setboxArrayUser(NewinputUser) 
+}
+
   return (
     <div className="app-wrapper">
       <h1 class="app-title">Message board</h1>
@@ -8,17 +30,20 @@ function MessageBoard() {
             id="message-text"
             name="message-text"
             type="text"
+            value={inputUser}
+            onChange={handleInputUser}
             placeholder="Enter message here"
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+        <button className="submit-message-button"onClick={handleBoxUser}>Submit</button>
       </div>
       <div class="board">
+      {boxArrayUser.map((text, index) => (
         <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
+          <h1>{text}</h1>
+          <button className="delete-button"onClick={deleteTheBox}>x</button>
         </div>
-      </div>
+      ))}</div>
     </div>
   );
 }
